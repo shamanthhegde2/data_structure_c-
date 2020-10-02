cfa6d9ac73f47050feed37633fe82e56b92bbd92
shamanth hegde <51316018+shamanthhegde2@users.noreply.github.com>
2020-10-02 07:18:57 +0530

    reorder-leet code

---
+++ reorder.cpp
@@ -0,0 +1,46 @@
+ListNode *reverse(ListNode *head)
+    {
+        ListNode *p{NULL};
+        ListNode *c{head};
+        ListNode *n;
+        while(c!=NULL)
+        {
+            n=c->next;
+            c->next=p;
+            p=c;
+            c=n;
+        }
+        return p;
+    }
+    void merge(ListNode *headA,ListNode *headB)
+    {
+        while(headA!=NULL)
+        {
+            ListNode *a=headA->next;
+            ListNode *b=headB->next;
+            headA->next=headB;
+            if(a==NULL)
+                break;
+            headB->next=a;
+            headA=a;
+            headB=b;
+        }
+    }
+    void reorderList(ListNode* head)
+    {
+        if(head==NULL || head->next==NULL)
+            return;
+        ListNode *fast{head};
+        ListNode *slow{head};
+        ListNode *prev;
+        while(fast!=NULL && fast->next!=NULL)
+        {
+            prev=slow;
+            fast=fast->next->next;
+            slow=slow->next;
+        }
+        prev->next=NULL;
+        ListNode *headA=head;
+        ListNode *headB=reverse(slow);
+        merge(headA,headB);
+    }
